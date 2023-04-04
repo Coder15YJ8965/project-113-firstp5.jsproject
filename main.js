4d52779c9c5df859ea49e459530c7d60638f2b05
Coder15YJ8965 <86280451+Coder15YJ8965@users.noreply.github.com>
2023-04-04 04:50:13 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+
+function setup() {
+        canvas = createCanvas(640, 480);
+        canvas.center();
+        video= createCapture(VIDEO);
+        video.hide();
+        background(255, 258, 213)
+}
+function draw() {
+    circle(30, 30, 20);
+    background()
+describe('white circle with black outline in mid of gray canvas');
+}
